7fd10cae714b58e375ce79cbe4ad2492c6f682bf
Yuval Varke <varke.vishu01@gmail.com>
2026-04-24 10:44:50 +0530

    Created small chunks from the Doc

--- index.py
+++ index.py
@@ -1,11 +1,17 @@
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 
 pdf_path = Path("Linux.pdf")
 
 #Now Load this file in the python program
-
 loader = PyPDFLoader(file_path=pdf_path)
 docs = loader.load()
 
-print(docs[2].page_content)
+# Split the doc into smaller chunks
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, 
+    chunk_overlap=400
+)
+chunks = text_splitter.split_documents(documents=docs)
